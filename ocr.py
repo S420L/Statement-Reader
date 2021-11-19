c9c7273f35c5ffebdaa7e2e8b9b7020021631085
Mike Hawk <michael.wicks42@gmail.com>
2021-11-19 14:40:38 -0500

    still working on column logic, reduced to 554 lines of code from high of 840

--- ocr.py
+++ ocr.py
@@ -52,7 +52,7 @@ def output_results():
 	data = list_to_dict(run_SQL(SQL))
 	send_to_excel(os.path.dirname(__file__),data,"Financial Statement Output",clear_indic='y',headings='y')
 
-def group_by_lines(image_lines):
+def group_lines(image_lines):
 	'''Iterative algorithm: 
 		-- if an element is too close to the top of the page to be in its line_num then move down one level
 		-- passed==0 if algorithm has converged
@@ -67,7 +67,27 @@ def group_by_lines(image_lines):
 						passed = 1
 	return image_lines, passed
 
-def left_rule(data):
+def fix_lines(image_lines):
+	'''part 2 of algorithm, fix data structure to force Python line index to equal line_num
+	   		-- force digits to be after words (TODO: flawed logic, should be ordered left to right)
+	'''
+	max_line_num = max([i['line_num'] for i in image_lines[-1]])
+	temp_list = [[] for i in range(0,max_line_num+1)]
+	for i in range(0,len(image_lines)):
+		for j in range(0,len(image_lines[i])):
+			temp_list[image_lines[i][j]['line_num']].append(image_lines[i][j])
+	return temp_list
+
+def group_by_lines(image_lines):
+	# group data into lines, sort left to right
+	for i in range(0,100):	
+		image_lines, passed = group_lines(image_lines)
+		image_lines = fix_lines(image_lines)
+		if(passed==0):
+			break
+	return image_lines
+
+def implement_left_rule(data):
 	'''Iterative algorithm:
 		-- makes sure data is clustered and there isn't data outside the columns
 		-- practically this would trigger if there's lots of space above or below a word
@@ -105,9 +125,9 @@ def left_rule(data):
 			continue
 	return data, passed
 
-def implement_left_rule(data):
+def left_rule(data):
 	for i in range(0,69):
-		data, passed = left_rule(data)
+		data, passed = implement_left_rule(data)
 		if(passed==0):
 			break
 	return data
@@ -139,17 +159,6 @@ def detect_gaps(data):
 			if((int(lefts[i+1])-int(lefts[i])) > gap_thresh):
 				results.append(int((int(lefts[i+1])-int(lefts[i]))/2 + int(lefts[i])))
 	return sorted(results, key = lambda num: num, reverse=False)
-
-def fix_image_lines(image_lines):
-	'''part 2 of algorithm, fix data structure to force Python line index to equal line_num
-	   		-- force digits to be after words (TODO: flawed logic, should be ordered left to right)
-	'''
-	max_line_num = max([i['line_num'] for i in image_lines[-1]])
-	temp_list = [[] for i in range(0,max_line_num+1)]
-	for i in range(0,len(image_lines)):
-		for j in range(0,len(image_lines[i])):
-			temp_list[image_lines[i][j]['line_num']].append(image_lines[i][j])
-	return temp_list
 
 def save_image_data(image_data):
 	'''save cleaned data from OCR program
@@ -212,12 +221,7 @@ def clean_numbers(data):
 def get_column_names(data, num_cols, high_top):
 	calendar_months = ['january','february','march','april','may','june','july','august','september','october','november','december']
 	left_thresh = 100
-	top_thresh=4
 	front,back = [],[]
-	for i in range(1,len(data)):
-		if(int(data[i]['top'])<int(data[i-1]['top'])+top_thresh): 
-			data[i]['top'] = data[i-1]['top']
-	data = sorted(data, key=lambda row: (int(row['top']), int(row['left'])))
 	possible_dates = []
 	for i in range(0,len(data)):
 		passed = 0
@@ -228,40 +232,32 @@ def get_column_names(data, num_cols, high_top):
 				break
 		if(passed==1):
 			try:
-				if(data[i+1]['text'].strip().replace(",","").replace(".","").isdigit() and data[i+2]['text'].strip().replace(",","").replace(".","").isdigit()):
-					possible_dates.append([data[i], data[i+1], data[i+2]])
+				one_out = data[i+1]['text'].strip().replace(",","").replace(".","")
+				two_out = data[i+2]['text'].strip().replace(",","").replace(".","")
+				three_out = data[i+3]['text'].strip().replace(",","").replace(".","")
+				try:
+					four_out = str(int(data[i+4]['text'].strip().replace(",","").replace(".","")))
+				except:
+					four_out = "420"
+				if(one_out.isdigit() and two_out.isdigit() and three_out.isdigit() and four_out.isdigit()):
+					if((int(two_out)>=1969 and int(two_out)<=2022) and (int(three_out)>=1969 and int(three_out)<=2022) and (int(four_out)>=1969 and int(four_out)<=2022)):
+						possible_dates = [[data[i], data[i+1], data[i+2]],[data[i], data[i+1], data[i+3]],[data[i], data[i+1], data[i+4]]]
+					elif((int(two_out)>=1969 and int(two_out)<=2022) and (int(three_out)>=1969 and int(three_out)<=2022)):
+						possible_dates = [[data[i], data[i+1], data[i+2]],[data[i], data[i+1], data[i+3]]]
+					else:
+						possible_dates.append([data[i], data[i+1], data[i+2]])
 				else:
 					raise Exception("Skip to next try...")
 			except:
 				try:
-					if(data[i+1]['text'].strip().replace(",","").isdigit()):
+					one_out = data[i+1]['text'].strip().replace(",","").replace(".","")
+					if(one_out.isdigit()):
 						possible_dates.append([data[i], data[i+1]])
-					else:
-						possible_dates.append([data[i]])
+						print(i)
 				except:
-					possible_dates.append([data[i]])
+					pass
 		if(len(possible_dates)==num_cols):
-			print(possible_dates)
-			entertain_data = [i for i in data if int(i['top'])>int(possible_dates[0][0]['top']) and int(i['top'])<=int(possible_dates[0][0]['top'])+50]
-			entertain_data = [item for sublist in entertain_data for item in sublist]
-			if(len(possible_dates)==num_cols):
-				if(len(possible_dates[0])==2):
-					for i in range(0,len(possible_dates)):
-						surface_1 = [int(possible_dates[i][0]['left']), int(possible_dates[i][-1]['left'])+int(possible_dates[i][-1]['width'])]
-						for j in range(0,len(entertain_data)):
-							surface_2 = [int(entertain_data[j]['left']), int(entertain_data[j]['left'])+int(entertain_data[j]['width'])]
-							if((surface_2[0]>=surface_1[0] and surface_2[0]<=surface_1[1]) or (surface_1[0]>=surface_2[0] and surface_1[0]<=surface_2[1])):
-								try:
-									number_below = int(entertain_data[j]['text'].strip().replace(",",""))
-									if(number_below>=1969 and number_below<=2022):
-										possible_dates[i].append(entertain_data[j])
-								except:
-									pass
-				columns = []
-				for i in possible_dates:
-					column = " ".join([j['text'] for j in i])
-					columns.append(column)
-				return columns
+			return [" ".join([j['text'] for j in i]) for i in possible_dates]
 
 	for i in data:
 		try:
@@ -317,22 +313,14 @@ def scrape_financials(image_data, page_num):
 	for i in range(0,len(image_lines[-1])):
 		image_lines[-1][i]['line_num'] = max_line_num
 
-	# group data into lines, sort left to right
-	for i in range(0,100):	
-		image_lines, passed = group_by_lines(image_lines)
-		image_lines = fix_image_lines(image_lines)
-		if(passed==0):
-			break
+	image_lines = group_by_lines(image_lines)
 	for i in range(0,len(image_lines)):
 		image_lines[i] = sorted(image_lines[i], key = lambda var: var['left'])
-
-	top_data = image_lines[0]+image_lines[1]
 
 	for i in range(0,len(image_lines)):
 		top_list = [k['top'] for k in image_lines[i]]
 		top_range = [min(top_list),max(top_list)]
-		image_lines[i] = {'text': [k['text'].replace("$","").replace(",","").strip().lower() for k in image_lines[i] if len(k['text'].replace("$","").strip().lower())>0],
-							'top': top_range, 'page_num': str(page_num+1)}
+		image_lines[i] = {'text': [k['text'].replace("$","").replace(",","").strip().lower() for k in image_lines[i] if len(k['text'].replace("$","").strip().lower())>0],'top': top_range, 'page_num': str(page_num+1)}
 
 	for i in range(0,len(image_lines)):
 		values = []
@@ -397,24 +385,21 @@ def scrape_financials(image_data, page_num):
 		"""
 	run_SQL(SQL, commit_indic='y')
 
-	# remove stuff that's floating (not in a column)
-	SQL = """select * from image_data_3 order by cast(left as 'decimal') asc;"""
-	data = run_SQL(SQL)
-	data = implement_left_rule(data)
-	data_dict = list_to_dict(data)
-	dict_to_sqlite(data_dict,"image_data_4")
-
 	# remove non-numeric data below a certain threshold on the page
-	SQL = """select * from image_data_4;"""
+	SQL = """select * from image_data_3;"""
 	data = run_SQL(SQL)
 	high_top = [i['top'] for i in sorted(data, key=lambda row: int(row['top']))]
 	high_top = int(high_top[int(len(high_top)/2)])
 	data = [i for i in data if i['text'].replace(",","").replace("$","").replace("(","").replace(")","").replace(" ","").replace("-","").replace("0.","").strip().lower().replace(".","").isdigit() or int(i['top'])<=high_top]
+	top_thresh=4
+	for i in range(1,len(data)):
+		if(int(data[i]['top'])<int(data[i-1]['top'])+top_thresh): 
+			data[i]['top'] = data[i-1]['top']
 	data_dict = list_to_dict(data)
-	dict_to_sqlite(data_dict,"image_data_5")
+	dict_to_sqlite(data_dict,"image_data_4")
 
 	# if there's a top discrepancy past a certain threshold boot from the line_num
-	SQL = """select * from image_data_5;"""
+	SQL = """select * from image_data_4;"""
 	data = run_SQL(SQL)
 	for i in range(0,len(data)):
 		line_num = data[i]['line_num']
@@ -423,10 +408,10 @@ def scrape_financials(image_data, page_num):
 		if(abs(current_top-min_top)>20):
 			data[i]['line_num'] = str(int(data[i]['line_num']) + 420)
 	data_dict = list_to_dict(data)
-	dict_to_sqlite(data_dict,"image_data_6")
+	dict_to_sqlite(data_dict,"image_data_5")
 
 	# part 2 of removing floating data
-	data = run_SQL("select * from image_data_6;")
+	data = run_SQL("select * from image_data_5;")
 	isolated = []
 	for i in range(0,69):
 		data, passed, isolated = isolate_whitespace(data, num_rows, isolated)
@@ -437,15 +422,15 @@ def scrape_financials(image_data, page_num):
 		dict_to_sqlite(list_to_dict(isolated),"garbage")
 	else:
 		run_SQL("delete from garbage;",commit_indic='y')
-	dict_to_sqlite(list_to_dict(data),"image_data_7")
+	dict_to_sqlite(list_to_dict(data),"image_data_6")
 
 	# throw out lines where # of data points exceeds # columns
 	SQL = """
 	insert into garbage
-	select * from image_data_7 where line_num in (
+	select * from image_data_6 where line_num in (
 	select distinct line_num from (
 	select distinct line_num, count(*) as count 
-	from image_data_7
+	from image_data_6
 	group by line_num 
 	order by count(*) asc)
 	where count>"""+str(num_cols)+""")
@@ -455,28 +440,28 @@ def scrape_financials(image_data, page_num):
 	run_SQL(SQL, commit_indic='y')
 	SQL = """
 	select a.* 
-	from image_data_7 a
+	from image_data_6 a
 	left join garbage b
 	on a.line_num=b.line_num and a.left=b.left and a.text=b.text
 	where b.line_num is null;
 	"""
-	dict_to_sqlite(list_to_dict(run_SQL(SQL)),"image_data_8")
+	dict_to_sqlite(list_to_dict(run_SQL(SQL)),"image_data_7")
 
-	# part 3 of removing floating data
-	SQL = """select * from image_data_8 order by cast(left as 'decimal') asc;"""
+	# remove "floating" data, make top value constant among similar data points
+	SQL = """select * from image_data_7 order by cast(left as 'decimal') asc;"""
 	data = run_SQL(SQL)
-	data = implement_left_rule(data)
+	data = left_rule(data)
 	data_dict = list_to_dict(data)
-	dict_to_sqlite(data_dict,"image_data_9")
+	dict_to_sqlite(data_dict,"image_data_8")
 
 	# bring in column numbers
-	SQL = """select distinct text, left, top from image_data_9 order by cast(left as 'decimal') asc;"""
+	SQL = """select distinct text, left, top from image_data_8 order by cast(left as 'decimal') asc;"""
 	data = run_SQL(SQL)
 	results = [0] + detect_gaps(data)
-	SQL = """drop table if exists image_data_10;"""
+	SQL = """drop table if exists image_data_9;"""
 	run_SQL(SQL, commit_indic='y')
 	SQL = """
-		create table image_data_10 as
+		create table image_data_9 as
 		select distinct variable, text, left, top, width, line_num, page_num, case
 		"""
 	counter = 1
@@ -485,51 +470,51 @@ def scrape_financials(image_data, page_num):
 		counter+=1
 	SQL += " when cast(left as 'decimal')>=" + str(results[-1]) + " then "+str(counter)
 	SQL += """ end as col_num
-		from image_data_9;
+		from image_data_8;
 		"""
 	run_SQL(SQL, commit_indic='y')
 
 	# force top value to be constant for each line
 	SQL = """
 		select distinct a.line_num, min(a.top) as top
-		from image_data_10 a
-		inner join image_data_9 b
+		from image_data_9 a
+		inner join image_data_8 b
 		on a.line_num=b.line_num and a.top<>b.top;
 		"""
 	data = run_SQL(SQL)
 	data = [i for i in data if i['line_num'] is not None]
 	for i in data:
 		conditional = "where line_num=" + str(i['line_num'])
-		update_in_table("image_data_10", "top", str(i['top']), conditional)
+		update_in_table("image_data_9", "top", str(i['top']), conditional)
 
 	# bring in column names
-	data = run_SQL("select * from image_data_10 order by cast(top as 'decimal') asc, cast(left as 'decimal');")	
+	data = run_SQL("select * from image_data_9 order by cast(top as 'decimal') asc, cast(left as 'decimal');")	
 	garbage = run_SQL("select * from garbage order by cast(top as 'decimal'), cast(left as 'decimal');")
 	columns = get_column_names(garbage, num_cols, int(high_top))
 	if(len(columns)==0):
 		columns = [i['text'] for i in data[0:num_cols]]
 		data = data[num_cols:]
-	print("_______________Final Column Defs: " + ", ".join(columns) + " _______________")
+	print("/n_______________Final Column Defs: " + ", ".join(columns) + " _______________/n")
 	for i in range(0,len(data)):
 		data[i]['column'] = columns[(int(data[i]['col_num'])-1)]
 	data = clean_numbers(data)
 	data_dict = list_to_dict(data)
-	dict_to_sqlite(data_dict,"image_data_11")
+	dict_to_sqlite(data_dict,"image_data_10")
 
 	# final table of clean data
-	SQL = """drop table if exists image_data_12;"""
+	SQL = """drop table if exists image_data_11;"""
 	run_SQL(SQL, commit_indic='y')
 	SQL = """
-		create table image_data_12 as
+		create table image_data_11 as
 		select distinct variable, cast(replace(text,'—','-') as 'decimal') as value, column, cast(page_num as 'decimal') as page_num,
 		cast(line_num as 'decimal') as line_num, cast(col_num as 'decimal') as col_num, cast(top as 'decimal') as top,
 		cast(left as 'decimal') as left, cast(width as 'decimal') as width
-		from image_data_11;
+		from image_data_10;
 		"""
 	run_SQL(SQL, commit_indic='y')
 	SQL = """
 	insert into financials
-	select * from image_data_12;
+	select * from image_data_11;
 	"""
 	run_SQL(SQL, commit_indic='y')
 
@@ -540,8 +525,8 @@ start_time = time.time()
 
 # if it's a PDF, convert to image first
 try:
-	images = pdf_to_image(filename)
 	print("PDF selected...")
+	images = pdf_to_image(filename)
 except:
 	print("IMAGE selected...")
 	images = [filename]
